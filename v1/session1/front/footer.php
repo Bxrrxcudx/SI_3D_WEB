e82f6880ec95d00e581db6d12ff583fccac52f01
barracuda75 <wilsai.mede@hetic.net>
2017-03-23 11:18:01 +0100

    Dossier include à jour

--- v1/session1/front/footer.php
+++ v1/session1/front/footer.php
@@ -1,20 +1,20 @@
-<footer>
-    <div class="social-networks">
-        <img src="img-layout/logo-facebook.png" alt="facebook">
-        <img src="img-layout/logo-insta.png" alt="insta">
-        <img src="img-layout/logo-twitter.png" alt="twitter">
-    </div>
-    <nav class="nav-footer">
-        <li><a href="">faq</a></li>
-        <li><a href="">mentions légales</a></li>
-        <li><a href="">contact</a></li>
-        <li><a href="">à propos</a></li>
-    </nav>
-    <div class="newsletter">
-        <form action="post">
-        <input class="newsletter-login" type="text" placeholder="Inscrivez-vous à la Newsletter !" id="newsletter">
-        </form>
-    </div>
-</footer>
+    <footer>
+        <div class="social-networks">
+            <img src="img-layout/logo-facebook.png" alt="facebook">
+            <img src="img-layout/logo-insta.png" alt="insta">
+            <img src="img-layout/logo-twitter.png" alt="twitter">
+        </div>
+        <nav class="nav-footer">
+            <li><a href="">faq</a></li>
+            <li><a href="">mentions légales</a></li>
+            <li><a href="">contact</a></li>
+            <li><a href="">à propos</a></li>
+        </nav>
+        <div class="newsletter">
+            <form action="post">
+            <input class="newsletter-login" type="text" placeholder="Inscrivez-vous à la Newsletter !" id="newsletter">
+            </form>
+        </div>
+    </footer>
 </body>
 </html>
--- v1/session1/front/header.php
+++ v1/session1/front/header.php
@@ -7,16 +7,16 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
 </head>
 <body>
-<header>
-    <div class="nav-side-user">
-      <img class="bag" src="img-layout/panier.png" alt="bag">
-    </div>
-    <nav class="nav-header">
-      <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="game.php">Pimp Your Ride</a></li>
-        <li><a href="">Nos Designs</a></li>
-        <li><a href="">Nos Accessoires</a></li>
-      </ul>
-    </nav>
-</header>
+    <header>
+        <div class="nav-side-user">
+          <img class="bag" src="img-layout/panier.png" alt="bag">
+        </div>
+        <nav class="nav-header">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="game.php">Pimp Your Ride</a></li>
+            <li><a href="">Nos Designs</a></li>
+            <li><a href="">Nos Accessoires</a></li>
+          </ul>
+        </nav>
+    </header>
