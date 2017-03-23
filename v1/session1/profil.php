4521539a4083346e3c19bcd83ac9926969c119ce
barracuda75 <wilsai.mede@hetic.net>
2017-03-23 12:44:20 +0100

    profil new

--- v1/session1/profil.php
+++ v1/session1/profil.php
@@ -14,20 +14,14 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 include "front/header.php"; 
 ?>	
 <?php if (isset($_SESSION['id'])) :?>
-<div class="connexion">
-	<a href="logout.php">Déconnexion</a>
-</div>
-<p>
-	Salut,
-	<?= $row['prenom']." ".$row['nom']?> et tu as <?= $row['age']?> ans !
-</p>
-<?php else :?>
-	<p>Vous n'êtes plus connecté</p>
-	<a href="signup.php">Inscris-toi</a>
-<?php endif; ?>
-	<main>
+<main>
 		<div class="profil">
 			<h2>Mon Profil</h2>
+			<div class="infos">
+				<p><?= $row['prenom']." ".$row['nom']?></p>
+				<p><?php  ?></p>
+				<p><?= $row['age']?> ans</p>
+			</div>
 			<figure>
 				<img src="https://placehold.it/450x300" alt="">
 				<figcaption>Fait le 12/12/12</figcaption>
@@ -59,6 +53,10 @@ include "front/header.php";
 			</div>
 		</div>
 	</main>
+<?php else :?>
+	<p>Vous n'êtes plus connecté</p>
+	<a href="signup.php">Inscris-toi</a>
+<?php endif; ?>
 <?php 
 include "front/footer.php";
 ?>
