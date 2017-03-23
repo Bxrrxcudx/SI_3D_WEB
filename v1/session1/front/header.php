75a79da437628b456d5561bb83ebd597cc469991
barracuda75 <wilsai.mede@hetic.net>
2017-03-23 16:09:36 +0100

    yeay

--- v1/session1/front/header.php
+++ v1/session1/front/header.php
@@ -5,6 +5,7 @@
     <title>My Urban Ride</title>
     <link rel="stylesheet" href="css/styles.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+    <script type='text/javascript' src='https://ssl-webplayer.unity3d.com/download_webplayer-3.x/3.0/uo/jquery.min.js'></script>
 </head>
 <body>
     <header>
@@ -23,10 +24,11 @@
         </div>
         <nav class="nav-header">
           <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Accueil</a></li>
             <li><a href="game.php">Pimp Your Ride</a></li>
             <li><a href="">Nos Designs</a></li>
             <li><a href="">Nos Accessoires</a></li>
+            <li><a href="concours.php">Jeu Concours</a></li>
           </ul>
         </nav>
     </header>
