95be2dfada5a876fd98029720795ff467d2140a7
barracuda75 <wilsai.mede@hetic.net>
2017-03-23 13:21:05 +0100

    game MAJ

--- v1/session1/game.php
+++ v1/session1/game.php
@@ -13,18 +13,6 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
 <?php 
 include "front/header.php";
 ?>
-<?php 
-	if (isset($_SESSION['id'])) {
-		echo '<div class="connexion">
-		<a href="logout.php">Déconnexion</a>
-		<a href="profil.php">Mon Profil</a>
-		</div>';
-	} 
-	else {
-		echo '<div class="connexion">
-		<a href="login.php">Connexion</a>';	
-	}
-?>
 <main>
 	<iframe src="ExternalCall_test.html" frameborder="0" scrolling="no"></iframe>
 	<a href="formulaire.php">Si tu n'as pas d'idée viens ici</a>
